c3814a8762ce26eb5e0c3c81d10d64ff45a9cc40
kiko <cahenriquesoares@gmail.com>
2023-08-03 00:05:01 -0300

    Deploy

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import streamlit as st
 
 credenciais = service_account.Credentials.from_service_account_info(
     {
-  "type": st.secrets['credenciais_google']['type'],
-  "project_id": st.secrets['credenciais_google']['project_id'],
-  "private_key_id": st.secrets['credenciais_google']['private_key_id'],
-  "private_key": st.secrets['credenciais_google']['private_key'],
-  "client_email": st.secrets['credenciais_google']['client_email'],
-  "client_id": st.secrets['credenciais_google']['client_id'],
-  "auth_uri": st.secrets['credenciais_google']['auth_uri'],
-  "token_uri": st.secrets['credenciais_google']['token_uri'],
-  "auth_provider_x509_cert_url": st.secrets['credenciais_google']['auth_provider_x509_cert_url'],
-  "client_x509_cert_url": st.secrets['credenciais_google']['client_x509_cert_url'],
-  "universe_domain": st.secrets['credenciais_google']['universe_domain']
+  "type": st.secrets['type'],
+  "project_id": st.secrets['project_id'],
+  "private_key_id": st.secrets['private_key_id'],
+  "private_key": st.secrets['private_key'],
+  "client_email": st.secrets['client_email'],
+  "client_id": st.secrets['client_id'],
+  "auth_uri": st.secrets['auth_uri'],
+  "token_uri": st.secrets['token_uri'],
+  "auth_provider_x509_cert_url": st.secrets['auth_provider_x509_cert_url'],
+  "client_x509_cert_url": st.secrets['client_x509_cert_url'],
+  "universe_domain": st.secrets['universe_domain']
     }
 )
 
